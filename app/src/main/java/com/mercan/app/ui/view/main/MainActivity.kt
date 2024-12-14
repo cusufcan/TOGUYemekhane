619c0f20242213ccdab83e72bc77385f962d2292
cusufcan <cusufcan@gmail.com>
2024-12-14 18:37:46 +0300

    First commit with <3

---
+++ app/src/main/java/com/mercan/app/ui/view/main/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.mercan.app.ui.view.main
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.mercan.app.R
+import com.mercan.app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        bindingActivityCodes()
+        defaultActivityCodes()
+    }
+
+    private fun defaultActivityCodes() {
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    private fun bindingActivityCodes() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
