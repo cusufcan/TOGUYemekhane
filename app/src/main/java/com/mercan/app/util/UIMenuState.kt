283322d2aaaf162f79968923e82ec906fce95540
cusufcan <cusufcan@gmail.com>
2024-12-23 18:27:36 +0300

    rename: UIState to UIMenuState

---
+++ app/src/main/java/com/mercan/app/util/UIMenuState.kt
@@ -0,0 +1,9 @@
+package com.mercan.app.util
+
+import com.mercan.app.data.model.MenuData
+
+sealed class UIMenuState {
+    data object Loading : UIMenuState()
+    data class Success(val menuData: MenuData) : UIMenuState()
+    data class Error(val message: String) : UIMenuState()
+}
--- app/src/main/java/com/mercan/app/util/UIState.kt
+++
@@ -1,9 +0,0 @@
-package com.mercan.app.util
-
-import com.mercan.app.data.model.ToguMenuData
-
-sealed class UiState {
-    data object Loading : UiState()
-    data class Success(val data: ToguMenuData) : UiState()
-    data class Error(val message: String) : UiState()
-}
