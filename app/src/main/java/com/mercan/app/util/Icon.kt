868c5b235acc82e025e6f3f35afeec13a4d3d14c
cusufcan <cusufcan@gmail.com>
2024-12-24 16:21:53 +0300

    rename: Icon enums

--- app/src/main/java/com/mercan/app/util/Icon.kt
+++ app/src/main/java/com/mercan/app/util/Icon.kt
@@ -3,9 +3,9 @@ package com.mercan.app.util
 import com.mercan.app.R
 
 enum class Icon(val icon: Int) {
-    SOUP(R.drawable.ic_soup),
-    MEAL_FIRST(R.drawable.ic_meal_first),
-    MEAL_SECOND(R.drawable.ic_meal_second),
-    SWEET(R.drawable.ic_sweet),
-    TOTAL_CALORIES(R.drawable.ic_total_calories),
+    SOUP(R.drawable.ic_soup_outlined),
+    MEAL_FIRST(R.drawable.ic_meal_first_outlined),
+    MEAL_SECOND(R.drawable.ic_meal_second_outlined),
+    SWEET(R.drawable.ic_sweet_outlined),
+    TOTAL_CALORIES(R.drawable.ic_total_calories_outlined),
 }
