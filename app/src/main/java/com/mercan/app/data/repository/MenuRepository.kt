8c72eab4beada6bd9dfbd503056b90ccc24c7ddc
cusufcan <cusufcan@gmail.com>
2024-12-24 16:23:20 +0300

    fix: Removed some log

--- app/src/main/java/com/mercan/app/data/repository/MenuRepository.kt
+++ app/src/main/java/com/mercan/app/data/repository/MenuRepository.kt
@@ -1,6 +1,5 @@
 package com.mercan.app.data.repository
 
-import android.util.Log
 import com.mercan.app.data.local.MenuLocalSource
 import com.mercan.app.data.model.MenuData
 import com.mercan.app.data.model.WeekData
@@ -14,12 +13,8 @@ class MenuRepository @Inject constructor(
     suspend fun getData(): MenuData {
         val remoteWeekData = menuRemoteSource.getWeekData()
         val localWeekData = menuLocalSource.getWeekData()
-// TODO: Silinecek
-        Log.i("MercanLogger", remoteWeekData.toString())
-        Log.i("MercanLogger", localWeekData.toString())
 
         if (remoteWeekData != null) {
-            Log.i("MercanLogger", remoteWeekData.startDate.plus(remoteWeekData.endDate))
             if (localWeekData == null || remoteWeekData.startDate != localWeekData.startDate) {
                 val webData = menuRemoteSource.getData()
                 menuLocalSource.saveData(webData)
