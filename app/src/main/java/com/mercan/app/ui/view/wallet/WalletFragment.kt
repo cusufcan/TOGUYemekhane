e95d55713069bd87342e541bb8ded05c5fd84a6e
cusufcan <cusufcan@gmail.com>
2024-12-24 16:18:58 +0300

    add: WalletFragment

---
+++ app/src/main/java/com/mercan/app/ui/view/wallet/WalletFragment.kt
@@ -0,0 +1,51 @@
+package com.mercan.app.ui.view.wallet
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.webkit.WebView
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import com.mercan.app.databinding.FragmentWalletBinding
+
+class WalletFragment : Fragment() {
+    private var _binding: FragmentWalletBinding? = null
+    private val binding get() = _binding!!
+
+    private lateinit var toolBar: Toolbar
+    private lateinit var webView: WebView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWalletBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        bindViews()
+        bindEvents()
+    }
+
+    private fun bindViews() {
+        toolBar = binding.toolBar
+        webView = binding.webView
+    }
+
+    private fun bindEvents() {
+        toolBar.setNavigationOnClickListener {
+            requireActivity().onBackPressedDispatcher.onBackPressed()
+        }
+
+        initWebView()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initWebView() {
+        webView.getSettings().javaScriptEnabled = true
+        webView.loadUrl("https://kampuskart.gop.edu.tr/User/Login")
+    }
+}
