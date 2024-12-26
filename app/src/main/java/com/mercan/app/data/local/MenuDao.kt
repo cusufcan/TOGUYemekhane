5e5e5d79e0963ab353cd90c3fbb0a0960c18d6a3
cusufcan <cusufcan@gmail.com>
2024-12-26 13:31:08 +0300

    fix: MenuDao onConflict

--- app/src/main/java/com/mercan/app/data/local/MenuDao.kt
+++ app/src/main/java/com/mercan/app/data/local/MenuDao.kt
@@ -2,6 +2,7 @@ package com.mercan.app.data.local
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.mercan.app.data.model.MenuData
 import com.mercan.app.data.model.WeekData
@@ -11,12 +12,12 @@ interface MenuDao {
     @Query("SELECT * FROM weekdata")
     suspend fun getWeekData(): WeekData?
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun saveWeekData(weekData: WeekData)
 
     @Query("SELECT * FROM menudata")
     suspend fun getData(): MenuData?
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun saveData(menuData: MenuData)
 }
