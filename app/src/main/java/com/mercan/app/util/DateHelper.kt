40a611a1c1202e91bb6f2ffa0262558a20d6191f
cusufcan <cusufcan@gmail.com>
2024-12-28 10:17:01 +0300

    fix: List item color on weekend

--- app/src/main/java/com/mercan/app/util/DateHelper.kt
+++ app/src/main/java/com/mercan/app/util/DateHelper.kt
@@ -5,7 +5,7 @@ import java.util.Calendar
 fun getDayOfWeek(): Int {
     val calendar = Calendar.getInstance()
     val dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
-    if (dayOfWeek == Calendar.SUNDAY || dayOfWeek == Calendar.SATURDAY) return 0
+    if (dayOfWeek == Calendar.SUNDAY || dayOfWeek == Calendar.SATURDAY) return -1
     return dayOfWeek - 2
 }
 
