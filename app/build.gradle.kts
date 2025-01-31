1b3bd6a6fc617256878998a73a6b759b88e39f34
cusufcan <cusufcan@gmail.com>
2025-01-31 21:29:22 +0300

    Bump versionCode to 21 and versionName to 3.0.4 in app build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.mercan.app"
         minSdk = 25
         targetSdk = 35
-        versionCode = 20
-        versionName = "3.0.3"
+        versionCode = 21
+        versionName = "3.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
