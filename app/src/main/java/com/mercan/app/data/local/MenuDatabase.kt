a858b4da605a21c4795dae8cb3504b4d368036b2
cusufcan <cusufcan@gmail.com>
2025-02-11 15:12:35 +0300

    feat: Bump MenuDatabase version to 4

--- app/src/main/java/com/mercan/app/data/local/MenuDatabase.kt
+++ app/src/main/java/com/mercan/app/data/local/MenuDatabase.kt
@@ -11,7 +11,7 @@ import com.mercan.app.data.model.WeekData
         MenuData::class,
         WeekData::class,
     ],
-    version = 3,
+    version = 4,
     exportSchema = false,
 )
 @TypeConverters(Converter::class)
