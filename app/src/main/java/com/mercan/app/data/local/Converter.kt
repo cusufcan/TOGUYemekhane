310daa6464b0b9195f064c20632ff87db0b103b1
cusufcan <cusufcan@gmail.com>
2025-02-11 15:12:10 +0300

    feat: Add WeekData conversion using Gson

--- app/src/main/java/com/mercan/app/data/local/Converter.kt
+++ app/src/main/java/com/mercan/app/data/local/Converter.kt
@@ -4,16 +4,28 @@ import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.mercan.app.data.model.MenuList
+import com.mercan.app.data.model.WeekData
 
 class Converter {
     @TypeConverter
     fun fromToguMenuList(value: MutableList<MenuList>): String {
-        return Gson().toJson(value) // Listeyi JSON'a çevirir
+        return Gson().toJson(value)
     }
 
     @TypeConverter
     fun toToguMenuList(value: String): MutableList<MenuList> {
         val type = object : TypeToken<MutableList<MenuList>>() {}.type
-        return Gson().fromJson(value, type) // JSON'u listeye çevirir
+        return Gson().fromJson(value, type)
+    }
+
+    @TypeConverter
+    fun fromWeekData(value: WeekData): String {
+        return Gson().toJson(value)
+    }
+
+    @TypeConverter
+    fun toWeekData(value: String): WeekData {
+        val type = object : TypeToken<WeekData>() {}.type
+        return Gson().fromJson(value, type)
     }
 }
